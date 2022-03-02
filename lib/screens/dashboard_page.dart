94ae8d4138b93bfe7201def2f4b30378eed7e5d1
Yosef Christian Mercene Cuenca <ymcuenca@cbzrc.pshs.edu.ph>
2022-03-03 02:44:17 +0800

    added folders and progress indicator for delete

--- lib/screens/dashboard_page.dart
+++ lib/screens/dashboard_page.dart
@@ -1,24 +1,14 @@
 import 'dart:async';
 
-import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
-import 'package:gsheets/gsheets.dart';
 import 'package:internet_connection_checker/internet_connection_checker.dart';
-import 'package:smart_taal_system/backend/enumeratorOfflineData.dart';
-import 'package:smart_taal_system/backend/enumeratorRawData.dart';
 import 'package:smart_taal_system/backend/google_sheets_api.dart';
 import 'package:smart_taal_system/backend/sqlfite_local_offline_cache.dart';
-import 'package:smart_taal_system/functions/check_connection.dart';
-import 'package:smart_taal_system/widgets/activities_list.dart';
+import 'package:smart_taal_system/widgets/lists/activities_list.dart';
 import 'package:smart_taal_system/widgets/calendar.dart';
-import 'package:smart_taal_system/widgets/offline_cache_list.dart';
+import 'package:smart_taal_system/widgets/lists/offline_cache_list.dart';
 import 'package:sqflite/sqflite.dart';
-import 'package:sqflite/sqlite_api.dart';
-import 'package:flutter/foundation.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
 import 'package:top_snackbar_flutter/custom_snack_bar.dart';
-import 'package:top_snackbar_flutter/tap_bounce_container.dart';
 import 'package:top_snackbar_flutter/top_snack_bar.dart';
 
 class Dashboard extends StatefulWidget {
@@ -54,18 +44,17 @@ class _DashboardState extends State<Dashboard> {
         await db.rawQuery('SELECT COUNT(*) FROM enumeratorOfflineData'));
     String countString = count.toString();
     var countInt = int.parse(countString);
-
     while (countInt != 0) {
       List<Map<String, dynamic>> results = await db.rawQuery(
           'SELECT * FROM enumeratorOfflineData WHERE id=?', ['$countInt']);
       for (var r in results) {
-        print(r);
         _postOnline(r);
         await sleep();
         await db.delete("enumeratorOfflineData", //table name
             where: "id = ?",
             whereArgs: [countInt] // use whereArgs to avoid SQL injection
             );
+        await sleep();
         setState(() {});
       }
       countInt--;
@@ -81,7 +70,7 @@ class _DashboardState extends State<Dashboard> {
   }
 
   Future sleep() {
-    return new Future.delayed(const Duration(milliseconds: 1500), () => "1");
+    return new Future.delayed(const Duration(milliseconds: 0), () => "1");
   }
 
   _postOnline(feedback) async {
@@ -93,20 +82,26 @@ class _DashboardState extends State<Dashboard> {
     return Scaffold(
         body: Container(
             margin:
-                EdgeInsets.only(top: MediaQuery.of(context).size.height / 100),
+                EdgeInsets.only(top: MediaQuery.of(context).size.height / 13),
             color: Colors.purple,
             child: RefreshIndicator(
                 onRefresh: () {
                   return Future.delayed(Duration(seconds: 1), () async {
                     setState(() {});
-
                     bool isConnected =
                         await InternetConnectionChecker().hasConnection;
                     if (isConnected == true) {
-                      print('Is connedted :)');
+                      print('Is connected :)');
                       _queryOffline();
                     } else {
                       print('Not connected :(');
+                      showTopSnackBar(
+                        context,
+                        CustomSnackBar.error(
+                          message:
+                              "Wala ka paring internet :( Kailangan ng internet para mag-sync ang datos sa database.",
+                        ),
+                      );
                     }
                   });
                 },
@@ -117,10 +112,12 @@ class _DashboardState extends State<Dashboard> {
                     child: SingleChildScrollView(
                         physics: BouncingScrollPhysics(
                             parent: AlwaysScrollableScrollPhysics()),
-                        child: Column(children: [
-                          OfflineCacheList(),
-                          Calendar(),
-                          ActivitiesList(),
-                        ]))))));
+                        child: Container(
+                            margin: EdgeInsets.only(top: 20),
+                            child: Column(children: [
+                              OfflineCacheList(),
+                              ActivitiesList(),
+                              Calendar(),
+                            ])))))));
   }
 }
